35a495c4bd6332c33e12f64acbf841898059ee3b
JustSock <sockincabbadge@gmail.com>
2022-04-22 00:18:53 +0500

    all done
    Yeah

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import siteFormCreateView from './view/site-form-create-view.js';
 import siteFormEditView from './view/site-form-edit-view.js';
 import siteWaypointView from './view/site-waypoint-view.js';
 import siteInfoView from './view/site-info-view.js';
+import siteEmptyView from './view/site-empty-view.js';
 import { getPoint } from './mockData/Waypoint.js';
 
 const waypointsNum = 17;
@@ -20,8 +21,14 @@ const siteEventsElement = document.querySelector('.trip-events');
 renderElement(siteMainElement, new siteInfoView(waypoints).element, RenderPosition.AFTERBEGIN);
 renderElement(siteMenuElement, new siteMenuView().element, RenderPosition.BEFOREEND);
 renderElement(siteFilterElement, new siteFilterView().element, RenderPosition.BEFOREEND);
-renderElement(siteEventsElement, new siteSortView().element, RenderPosition.BEFOREEND);
-renderElement(siteEventsElement, new siteEventsView().element, RenderPosition.BEFOREEND);
+if (waypoints?.length > 0){
+  renderElement(siteEventsElement, new siteSortView().element, RenderPosition.BEFOREEND);
+  renderElement(siteEventsElement, new siteEventsView().element, RenderPosition.BEFOREEND);
+}
+else{
+  const text='Click New Event to create your first point';
+  renderElement(siteEventsElement, new siteEmptyView(text).element, RenderPosition.BEFOREEND);
+}
 
 const siteEventElement = siteEventsElement.querySelector('.trip-events__list');
 if (waypoints?.length>0){
@@ -33,14 +40,31 @@ for (let k = 0; k < waypoints?.length; k++) {
   const formEdit=new siteFormEditView(waypoints[k]);
   const wayPoint=new siteWaypointView(waypoints[k]);
 
+  const onEscKeyDown=(evnt)=>{
+    if (evnt.key==='Esc'||evnt.key==='Escape'){
+      evnt.preventDefault();
+      siteEventElement.replaceChild(wayPoint.element, formEdit.element);
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   wayPoint.element.querySelector('.event__rollup-btn').addEventListener('click', ()=>{
     siteEventElement.replaceChild(formEdit.element, wayPoint.element);
+    document.addEventListener('keydown', onEscKeyDown);
   });
 
   formEdit.element.querySelector('form').addEventListener('submit', (x)=>{
     x.preventDefault();
     siteEventElement.replaceChild(wayPoint.element, formEdit.element);
+    document.addEventListener('keydown', onEscKeyDown);
   });
+
+  formEdit.element.querySelector('.event__rollup-btn').addEventListener(
+    'click', ()=>{
+      siteEventElement.replaceChild(wayPoint.element, formEdit.element);
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  );
 
   renderElement(siteEventElement, wayPoint.element, RenderPosition.BEFOREEND);
 }
